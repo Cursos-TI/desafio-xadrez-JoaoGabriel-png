e3d509c6af23ff0cfbac47cd9e8ed7f1861960c7
JoaoGabriel-png <227243817+JoaoGabriel-png@users.noreply.github.com>
2025-10-29 09:24:40 -0300

    Atualizar o xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -4,11 +4,49 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-    int a, b;
-    
+//nem sei se tinha alguma outra forma, mas funcionou
+void mova(int a){
+        if (a > 0){
+            printf("Direita\n");
+            mova(a - 1);
+        }
+    }
 
+void movaB(int b){
+        if (b > 0){
+            printf("Esquerda\n");
+            movaB(b - 1);
+        }
+    }
+
+void movaC(int c){
+        if (c > 0){
+            printf("Direita e Cima\n");
+            movaC(c - 1);
+        }
+    }
+
+int main() {
+    int C;
+
+    printf("Torre: \n");
+    mova(5);
+
+    printf("\nRainha: \n");
+    movaB(8);
+
+    printf("\nBispo: \n");
+    movaC(5);
+
+//Nível Aventureiro
+    printf("\nCavalo: \n");
+    {
+        while (C--);
+            for (int i = 0; i != 2; i++)
+        {
+            printf("Cima\n");
+        }
+        printf("Direita\n");
+    }
     return 0;
 }
